bc3b2948d6e53e839ce1720660dce9db0409ff85
Danil <danilstasilevich@yandex.ru>
2021-03-20 17:45:04 +0700

    Добавил ввод текста

---
+++ theBatyaTypeGame/Assets/Danil/Scripts/InputLetters.cs
@@ -0,0 +1,55 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Danil.Scripts
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class InputLetters : MonoBehaviour
+    {
+        [SerializeField] private AudioClip _audioClipRight;
+        [SerializeField] private AudioClip _audioClipWrong;
+        
+        private AudioSource _audioSource;
+        private TMP_InputField _inputField;
+
+        private void Start()
+        {
+            _inputField = GetComponent<TMP_InputField>();
+            _inputField.onEndEdit.AddListener(OnEndEdit);
+            //_inputField.onTextSelection.AddListener(OnTextSelection);
+            _inputField.ActivateInputField();
+            _inputField.onValueChanged.AddListener(OnValueChanged);
+        }
+        
+        private void OnEndEdit(string str)
+        {
+            _inputField.text = "";
+            _inputField.ActivateInputField();
+        }
+
+        public void OnAnswer(bool right)
+        {
+            if (right)
+            {
+                _audioSource.clip = _audioClipRight;
+                _audioSource.Play();
+            }
+            else
+            {
+                _audioSource.clip = _audioClipWrong;
+                _audioSource.Play();
+            }
+        }
+        
+        private void OnValueChanged(string str)
+        {
+            //_audioSource.Play();
+        }
+
+        private void OnTextSelection(string str, int index1, int index2)
+        {
+
+        }
+    }
+}
