505d977e19168fe7ff07780bbfe5aed0bec51a30
GoodComrad <konnov-v@mail.eu>
2021-03-20 18:43:54 +0100

    Добавил генератор условий + метод сравнения введенной и целевой строк

---
+++ theBatyaTypeGame/Assets/Danil/Scripts/Scriptables/AllTypesOfSprites.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Danil.Scripts.Scriptables
+{
+    [CreateAssetMenu(fileName = "AllTypes", menuName = "Danil/AllTypes", order = 0)]
+    public class AllTypesOfSprites : ScriptableObject
+    {
+        public List<Sprites> SpriteTypes;
+    }
+}
---
+++ theBatyaTypeGame/Assets/Danil/Scripts/TimeManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace Danil.Scripts
+{
+    public class TimeManager : MonoBehaviour
+    {
+        [SerializeField] private SpriteChanger _spriteChanger;
+        [SerializeField] private InputLetters _inputLetters;
+        [SerializeField] private float _reduce = 1.0f;
+        [SerializeField] private float _startTime = 6.0f;
+        
+        private Timer _timer;
+
+        private float _spritesChangeCount;
+
+        private void Start()
+        {
+            _timer = new Timer(_startTime);
+            _inputLetters.OnAction += OnAction;
+            _spritesChangeCount = _spriteChanger.GetSpritesCount() / _timer.CurrentTime;
+        }
+
+        private void Update()
+        {
+            _timer.TimerTick(Time.deltaTime);
+
+            if (_spritesChangeCount > (_timer.TimerMaxValue - _timer.CurrentTime) / _spriteChanger.GetSpritesCount())
+            {
+                Debug.Log("changed");
+                _spriteChanger.ChangeSprite();
+            }
+
+            //if (_timer.CurrentTime < _timer.TimerMinValue)
+            //{
+            //    Debug.Log("You lose!");
+            //}
+        }
+
+        private void OnAction(bool right)
+        {
+            if (!right)
+            {
+                _timer.StopTimeCount();
+            }
+            else
+            {
+                _timer = new Timer(0, _timer.TimerMaxValue - _reduce);
+            }
+        }
+    }
+}
